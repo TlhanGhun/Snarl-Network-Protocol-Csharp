ab7e13b695c6c1168c6483404242a48d903e95ef
ranveer5289 <ranveer.raghu@gmail.com>
2012-08-09 18:39:51 +0530

    Hello World Notification example

--- HelloSnarl.cs
+++ HelloSnarl.cs
@@ -11,24 +11,30 @@ class HelloSnarl
         string hostname = "127.0.0.1";
         int hostport = 9887;
         string appName = "HelloSnarlApp";
-
         string icon = "http://a0.twimg.com/profile_images/1100695109/snarl_logo_2008a_normal.png";
-
-        SNP snarl_object = new SNP(hostname, hostport);
-
-        snarl_object.register(appName);
-
         string title = "Notification from";
         string message = "HelloSnarl";
         string timeout = "5";
 
-        snarl_object.notify(appName, null, title, message, timeout, icon);
+        SNP snarl_object = new SNP(hostname, hostport);
+
+        if (snarl_object.isSnarlRunning())
+        {
+            Console.WriteLine("Yes snarl is running");
+
+            snarl_object.register(appName);
+            snarl_object.notify(appName, null, title, message, timeout, icon);
+
+            /*According to Snarl Developer Guide "When your application exits, it should unregister with Snarl."
+              If you want you can remove snarl_object.unregister because when you
+              quit snarl it will automatically unregister your application.*/
+
+            snarl_object.unregister(appName);
+        }
+        else
+            Console.WriteLine("Snarl not running");
 
 
-        /*According to Snarl Developer Guide "When your application exits, it should unregister with Snarl."
-        If you want you can remove snarl_object.unregister because when you
-        quit snarl it will automatically unregister your application.*/
-        snarl_object.unregister(appName);
 
     }
 
